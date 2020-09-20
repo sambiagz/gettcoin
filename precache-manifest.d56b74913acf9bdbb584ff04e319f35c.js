3fcbbdad187f0847d630b2f4697cb5ded68be001
sambiagz <61049353+sambiagz@users.noreply.github.com>
2020-09-20 03:02:16 +0300

    Add files via upload

---
+++ precache-manifest.d56b74913acf9bdbb584ff04e319f35c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb80e4299cca1cc2aa9fa1d542fbc53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0e75c63ef535db22cf",
+    "url": "/static/js/2.d711b235.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.d711b235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0b208b006658b7af0c",
+    "url": "/static/js/main.ae75426b.chunk.js"
+  },
+  {
+    "revision": "de9d7b2868e288e79b25",
+    "url": "/static/js/runtime-main.bb9ee77d.js"
+  },
+  {
+    "revision": "8764e73c6dd4844b3f92ed205c52c0be",
+    "url": "/static/media/mylogo.8764e73c.ico"
+  }
+]);
